ecdd42ecbc9881cdcfaf4fc9e9b064e7ba68549b
valentine <valentin.paramonov@protonmail.com>
2017-10-07 10:26:33 +0300

    Move EnvironmentVariableProvider to env package

--- src/main/java/paramonov/valentine/filemover/Main.java
+++ src/main/java/paramonov/valentine/filemover/Main.java
@@ -2,7 +2,7 @@ package paramonov.valentine.filemover;
 
 import paramonov.valentine.filemover.server.TomcatServer;
 
-import static paramonov.valentine.filemover.EnvironmentVariableProvider.getIntEnv;
+import static paramonov.valentine.filemover.env.EnvironmentVariableProvider.getIntEnv;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/paramonov/valentine/filemover/env/EnvironmentVariableProvider.java
+++ src/main/java/paramonov/valentine/filemover/env/EnvironmentVariableProvider.java
@@ -1,17 +1,17 @@
-package paramonov.valentine.filemover;
+package paramonov.valentine.filemover.env;
 
 import java.util.Optional;
 
 /**
  * Utility class to query environment variables
  */
-class EnvironmentVariableProvider {
+public class EnvironmentVariableProvider {
     /**
      * @param var          name of the environment variable (case-sensitive)
      * @param defaultValue the default value to use the variable doesn't exist
      * @return the value of the variable, converted to int
      */
-    static int getIntEnv(String var, int defaultValue) {
+    public static int getIntEnv(String var, int defaultValue) {
         return Optional
             .ofNullable(System.getenv(var))
             .map(Integer::valueOf)
