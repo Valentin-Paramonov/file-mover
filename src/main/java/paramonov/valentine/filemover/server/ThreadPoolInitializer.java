fbfde86c6929a928009cadc4e80810dc2a82ac34
valentine <valentin.paramonov@protonmail.com>
2017-10-09 09:49:51 +0300

    Document ThreadPoolInitializer

--- src/main/java/paramonov/valentine/filemover/server/ThreadPoolInitializer.java
+++ src/main/java/paramonov/valentine/filemover/server/ThreadPoolInitializer.java
@@ -8,8 +8,16 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 
+/**
+ * Initializes a {@link ScheduledThreadPoolExecutor} of {@value #poolSize} daemon
+ * threads with priority {@value #threadPriority} on context startup and stores it as a
+ * context attribute under the key {@value paramonov.valentine.filemover.server.ContextAttribute#threadPool}
+ */
 @WebListener
 public class ThreadPoolInitializer implements ServletContextListener {
+    private static final int poolSize = 4;
+    private static final int threadPriority = 4;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
@@ -18,10 +26,10 @@ public class ThreadPoolInitializer implements ServletContextListener {
 
     private Executor scheduledThreadPool() {
         return new ScheduledThreadPoolExecutor(
-            4,
+            poolSize,
             runnable -> {
                 Thread thread = new Thread(runnable);
-                thread.setPriority(4);
+                thread.setPriority(threadPriority);
                 thread.setDaemon(true);
                 return thread;
             }
