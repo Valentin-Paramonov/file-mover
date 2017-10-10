d79eb3f0c77bd26e1fb574489d6b5f9d253dcad0
valentine <valentin.paramonov@protonmail.com>
2017-10-10 09:28:32 +0300

    Document ServerStartException

--- src/main/java/paramonov/valentine/filemover/server/ServerStartException.java
+++ src/main/java/paramonov/valentine/filemover/server/ServerStartException.java
@@ -1,5 +1,8 @@
 package paramonov.valentine.filemover.server;
 
+/**
+ * Used for signaling that something went wrong during the server startup
+ */
 public class ServerStartException extends RuntimeException {
     ServerStartException(Exception e) {
         super(e);
